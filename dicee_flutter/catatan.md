eb4f9d0b222511009cb8fe93403a2fa1e1f88200
indraworks <indra.works2020@gmail.com>
2021-08-31 12:34:25 +0700

    2nd commit

---
+++ dicee_flutter/catatan.md
@@ -0,0 +1,4 @@
+https://medium.com/flutter-community/flutter-layout-cheat-sheet-5363348d037e
+
+animation:
+https://medium.com/flutter-community/flutter-animations-cheat-sheet-7f8cebfb850c
---
+++ dicee_flutter/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyDicee());
+}
+
+
+class MyDicee extends StatelessWidget {
+  //const MyDicee({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
